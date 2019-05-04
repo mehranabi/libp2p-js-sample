19106c1b042c940e6afa33b108dbc05773530890
Mehran Abghari <mehran.ab80@gmail.com>
2019-05-04 15:59:41 +0430

    add secio & multiplex modules

--- src/p2p/index.js
+++ src/p2p/index.js
@@ -3,9 +3,14 @@ const TCP = require('libp2p-tcp')
 
 const defaultsDeep = require('@nodeutils/defaults-deep')
 
+const Multiplex = require('libp2p-mplex')
+const SECIO = require('libp2p-secio')
+
 const DEFAULT_OPTS = {
   modules: {
     transport: [TCP],
+    connEncryption: [SECIO],
+    streamMuxer: [Multiplex],
   },
 }
 
